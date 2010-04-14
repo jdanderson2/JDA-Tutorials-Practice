7970c9b01a33274569de911a12be8c5208cec295
JeoAnd <jeo.anderson2@gmail.com>
2010-04-14 10:15:22 -0500

    add stats analysis add stats analysis

---
+++ Statistical Analysis/Simple statistics.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+from optparse import Values
+
+
+class stats
+{
+    private:
+        std:vector <double> V;
+        double mean, median, mode;
+    public:
+        stats(void); 
+        void sort_vect(void);
+        void run_stats(void);
+        double get_mean(void);
+        double get_meadian(void);
+        double get_mode(void);
+
+
+};
+
+
+int main()
+{
+    stats my_stats;
+    my_stats.sort_vect();
+    my_stats.run stats();
+    std::cout << "Here are the values of the statistical analysis"
+    std::cout << "Mean = "
+}
+
+
+int main()
+{
+    return
+}
